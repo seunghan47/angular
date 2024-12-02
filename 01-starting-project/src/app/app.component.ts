fd4bc49d1584def2683e99f65aac73e601f5d0e3
Paul Lim <paulslim47@gmail.com>
2024-12-02 17:19:43 -0500

    header

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from './components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
