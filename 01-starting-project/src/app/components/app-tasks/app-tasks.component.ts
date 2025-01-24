4d2eeee2b2289be4d2b10b0aa1fc75dc969002a6
Paul Lim <paulslim47@gmail.com>
2025-01-24 08:48:12 -0500

    dynamic tasks

---
+++ 01-starting-project/src/app/components/app-tasks/app-tasks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-tasks.component.html',
+  styleUrl: './app-tasks.component.css'
+})
+export class AppTasksComponent {
+
+}
